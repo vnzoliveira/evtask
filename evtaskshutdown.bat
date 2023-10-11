d898828cf94a126d726911e6f9936311bdc479b6
Vinicius de Oliveira <82851863+vnzoliveira@users.noreply.github.com>
2023-10-11 04:33:59 -0300

    Update evtaskshutdown.bat to find .14 version of Tomcat

--- evtaskshutdown.bat
+++ evtaskshutdown.bat
@@ -1,6 +1,6 @@
 @echo off
 ::Configs
-set TOMCAT_DIR=C:\Program Files\apache-tomcat-10.1.13
+set TOMCAT_DIR=C:\Program Files\apache-tomcat-10.1.14
 
 ::Shuts down Tomcat server
 cd "%TOMCAT_DIR%\bin"
